1e8a122510b62fa1f371b121f57e85dbe55de15c
NikitaErin <erinnikita654@gmail.com>
2026-05-31 17:01:41 +0300

    Use separate GOOGLE_CLIENT_EMAIL and GOOGLE_PRIVATE_KEY env vars

--- server.js
+++ server.js
@@ -41,7 +41,10 @@ const SHEETS = {
 };
 
 const sheetsAuth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY || '{}'),
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+  },
   scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
 });
 const sheetsApi = google.sheets({ version: 'v4', auth: sheetsAuth });
